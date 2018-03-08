f91fab92eddae2a9b2442d180a03e19c53cfeb2f
bhuvnesht26 <bhuvnesht26@gmail.com>
2018-03-08 17:42:01 +0530

    header footer

---
+++ js/header.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+            renderHeader();
+            renderDrawer();
+});
+
+function renderHeader() {
+    
+    document.getElementById("header-container").innerHTML = `
+    
+        <div class="mdl-layout__header-row">
+            <span class="android-title mdl-layout-title">
+                <img class="android-logo-image" src="images/android-logo.png">
+            </span>
+          <!-- Add spacer, to align navigation to the right in desktop -->
+            <div class="android-header-spacer mdl-layout-spacer"></div>
+          
+          <!-- Navigation -->
+            <div class="android-navigation-container">
+            <nav class="android-navigation mdl-navigation">
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="index.html">Top Headlines</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="business.html">Business</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="politics.html">Politics</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="sports.html">Sports</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="entertainment.html">Entertainment</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Contests</a>
+            </nav>
+          </div>
+          <span class="android-mobile-title mdl-layout-title">
+            <img class="android-logo-image" src="images/android-logo.png">
+          </span>
+        </div>
+        `;
+}
+
+function renderDrawer() {
+    
+    document.getElementById("header-drawer").innerHTML = `
+    
+        <span class="mdl-layout-title">
+          <img class="android-logo-image" src="images/android-logo-white.png">
+        </span>
+        <nav class="mdl-navigation">
+              <a class="mdl-navigation__link " href="index.html">Top Headlines</a>
+              <a class="mdl-navigation__link " href="business.html">Business</a>
+              <a class="mdl-navigation__link " href="politics.html">Politics</a>
+              <a class="mdl-navigation__link " href="sports.html">Sports</a>
+              <a class="mdl-navigation__link " href="entertainment.html">Entertainment</a>
+              <a class="mdl-navigation__link " href="contests.html">Contests</a>
+          <div class="android-drawer-separator"></div>
+          <span class="mdl-navigation__link" href="">Privacy Policy</span>
+          
+        </nav>
+        `;
+}
---
+++ js/index.js
@@ -0,0 +1,116 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', url);
+    ourRequest.onload = function () {
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        // This is where we'll do something with the retrieved data
+            var ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+            console.log(pageheading);
+            renderPageHeading();
+            } 
+        else {
+                    console.log("We connected to the server, but it returned an error.");
+                }
+            };
+
+    ourRequest.onerror = function() {
+        console.log("Connection error");
+        };
+
+    ourRequest.send();
+
+});
+
+function newsTemplate(news) {
+    
+    return `
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="${news["Articles Urltoimage"]}">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">${news["Articles Title"]}</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">${news["Articles Description"]}</span>
+              </div>
+              <div class="mdl-card__actions">
+                 <a class="android-link mdl-button mdl-js-button mdl-typography--font-light" href="${news["Articles Url"]}" target="_blank">
+                   ${news["Articles Source Name"]}
+                 </a>
+              </div>
+            </div>    
+        `;
+}
+
+function renderHTML(htmlData) {
+document.getElementById("news-container").innerHTML = `
+  ${htmlData.map(newsTemplate).join("")}
+`;
+}
+
+function renderPageHeading() {
+    document.getElementById("page-heading").innerHTML = pageheading;
+}
+
+var pageName = (function () {
+        var a = window.location.href,
+            b = a.lastIndexOf("/");
+        return a.substr(b + 1);
+    }());
+
+var url = (function (pageName) {
+    
+    switch(pageName) {
+    case "index.html":
+        url = "https://bhuvnesht26.github.io/api/index-api.json";
+        break;
+    case "business.html":
+        url = "https://bhuvnesht26.github.io/api/business-api.json";
+        break;
+    case "entertainment.html":
+        url = "https://bhuvnesht26.github.io/api/entertainment-api.json";
+        break;
+    case "health.html":
+        url = "https://bhuvnesht26.github.io/api/health-api.json";
+        break;
+    case "politics.html":
+        url = "https://bhuvnesht26.github.io/api/politics-api.json";
+        break;
+    case "sports.html":
+        url = "https://bhuvnesht26.github.io/api/sports-api.json";
+        break;
+    default:
+        url = "https://bhuvnesht26.github.io/api/index-api.json";
+}
+    return url;
+}());
+
+var pageheading = (function (){
+    
+    switch(pageName) {
+    case "index.html":
+        pageheading = "Top Headlines";
+        break;
+    case "business.html":
+        pageheading = "Top Business News";
+        break;
+    case "entertainment.html":
+        pageheading = "Top Entertainment News";
+        break;
+    case "health.html":
+        pageheading = "News to keep you Fit";
+        break;
+    case "politics.html":
+        pageheading = "Top Political News";
+        break;
+    case "sports.html":
+        pageheading = "Sports Feed";
+        break;
+    default:
+        pageheading = "Top Headlines";
+}
+    return pageheading;
+}());
